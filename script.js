b4f7e64596825ee5b7d017b4b5e5e8cf0e8d8b4e
SaschaPetro <petrovits1987@gmail.com>
2022-09-03 12:42:20 +0200

    init

---
+++ script.js
@@ -0,0 +1,14 @@
+function changeColor() {
+  let red = document.querySelector("#slider-red").value;
+  let green = document.querySelector("#slider-green").value;
+  let blue = document.querySelector("#slider-blue").value;
+  let color = "rgb(" + red + "," + green + "," + blue + ")";
+  document.body.style.backgroundColor = color;
+  document.querySelector("#color-value").innerHTML = " " + color;
+}
+
+document.querySelector("#slider-red").addEventListener("input", changeColor);
+
+document.querySelector("#slider-green").addEventListener("input", changeColor);
+
+document.querySelector("#slider-blue").addEventListener("input", changeColor);
